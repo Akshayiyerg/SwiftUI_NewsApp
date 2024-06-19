8ade39306ed72dc3b67c0733bde02d46e8b694b8
Akshay Ganesh <akshayiyerg@gmail.com>
2024-06-19 18:00:42 -0400

    Initail project setting commit

---
+++ SwiftUI_NewsAPP/SwiftUI_NewsAPP/SwiftUI_NewsAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NewsAPPApp.swift
+//  SwiftUI_NewsAPP
+//
+//  Created by Akshay  on 2024-06-19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NewsAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
